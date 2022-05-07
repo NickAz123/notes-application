630e6b6bf15ec6eda282ebfafd676477aaad9919
Nick <nick.azurin@gmail.com>
2022-05-07 13:50:49 -0700

    modified listItems to trim titles and generate dates for preview

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,11 +1,43 @@
+import { getByTitle } from "@testing-library/react";
 import React from "react";
 import { Link } from "react-router-dom";
+
+const generateTitle = (note) => {
+  //splitting title by new lines
+  const title = note.body.split("\n")[0];
+
+  if (title.length > 45) {
+    return title.slice(0, 45);
+  }
+
+  return title;
+};
+
+const generateTime = (note) => {
+  return new Date(note.updated).toLocaleDateString();
+};
+
+const generatePreview = (note) => {
+  const title = generateTitle(note);
+  let preview = note.body.replaceAll("\n", "");
+  
+  if (preview.length > 45) {
+    preview = preview.replaceAll(title, "");
+    return preview.slice(0, 45) + "...";
+  }
+
+  return preview;
+};
 
 const ListItem = ({ note }) => {
   return (
     <Link to={`/note/${note.id}`}>
       <div className="notes-list-item">
-        <h3>{note.body}</h3>
+        <h3>{generateTitle(note)}</h3>
+        <p>
+          <span>{generateTime(note)} | </span>
+          {generatePreview(note)}
+        </p>
       </div>
     </Link>
   );
